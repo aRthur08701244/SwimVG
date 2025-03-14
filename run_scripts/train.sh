8622336f53534570241a52a5dbb7bcf36729600c
ltShi <165351412+ltShi@users.noreply.github.com>
2025-03-14 14:41:01 +0800

    Add files via upload

--- run_scripts/train.sh
+++ run_scripts/train.sh
@@ -2,14 +2,15 @@
 dataset_name="refcoco+" 
 config_name="swimvg_dinov2.yaml"
 
+gpu="7"
+np=$(echo $gpu | tr -cd ',' | wc -c)
+np=$((np + 1))
+omp=8
 
-np=1
-omp=16
+MASTER_PORT=29560
 
-CUDA_VISIBLE_DEVICES=7 \
-MASTER_PORT=29560 \
 OMP_NUM_THREADS=$omp \
-
+CUDA_VISIBLE_DEVICES=$gpu \
 torchrun --nproc_per_node=$np --master_port=$MASTER_PORT ./train.py \
 --config config/$dataset_name/$config_name \
 --aug_crop --aug_scale --aug_translate \
